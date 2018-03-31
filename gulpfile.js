69e9ec0d3f06f9b9be44236d75c1224ecc752607
Sina Ghabeli <sinagh30@gmail.com>
2018-03-31 14:03:18 +0200

    Gulp File

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,8 @@ gulp.task('message', function() {
   return console.log('Gulp is running');
 });
 
-gulp.task('default', function() {
-  return console.log('Gulp is running');
+// Copy All HTML Files
+gulp.task('copyHTML', function() {
+  gulp.src('src/*.html')
+    .pipe(gulp.dest('dist'));
 });
